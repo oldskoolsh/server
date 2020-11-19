9eb935ffab12319310832f63e526190d4d2a62b2
Ricardo Pardini <ricardo@pardini.net>
2020-11-19 11:15:14 +0100

    adding jest, via ts-jest, I might regret this

--- src/test.ts
+++ src/test.ts
@@ -1,46 +1,39 @@
 import {RepoResolver} from "./repo/resolver";
-import {Repository} from "./repo/repo";
 import {RenderingContext} from "./repo/context";
-import {CloudInitRecipeListExpander} from "./expander_merger/ci_expander";
-import {Recipe} from "./repo/recipe";
 import {CloudInitProcessorStack} from "./processors/stack";
 import YAML from 'yaml';
 import {TedisPool} from "tedis";
 import {DefaultGeoIPReaders} from "./shared/geoip";
 import {CloudInitExpanderMerger} from "./expander_merger/expandermerger";
-import exp from "constants";
 
 
-async function faz() {
+test('simple expander/merger run', async () => {
     const resolver = new RepoResolver("/Users/pardini/Documents/Projects/github/oldskool", "oldskool-rpardini");
-    let repo: Repository = await resolver.rootResolve();
-
-    let context = new RenderingContext("https://cloud-init.pardini.net/", new TedisPool(), await (new DefaultGeoIPReaders()).prepareReaders());
-    await context.init();
-    context.clientIP = "62.251.42.9";
+    await resolver.rootResolve();
+    let tedisPool = new TedisPool();
     try {
+        let geoipReaders = await (new DefaultGeoIPReaders()).prepareReaders();
+        let context = new RenderingContext("https://cloud-init.pardini.net/", tedisPool, geoipReaders);
+        await context.init();
+
+        context.clientIP = "62.251.42.9";
+
         // bash render.
         // let rendered = await (new BashScriptAsset(context, resolver, "base.sh")).renderFromFile();
 
-
         // initial expansion.
-        let initialRecipes:string[] = ['k8s'];
+        let initialRecipes: string[] = ['k8s'];
 
         let expanderMerger: CloudInitExpanderMerger = new CloudInitExpanderMerger(context, resolver, initialRecipes);
         let smth = await expanderMerger.process();
 
         // processors run when everything else is already included.
         let finalResult = await new CloudInitProcessorStack(context, resolver, smth).addDefaultStack().process();
-        await console.log("finalResult\n---", YAML.stringify(finalResult, {}), "\n---");
-
-
+        let yaml = YAML.stringify(finalResult, {});
     } finally {
-        await context.logClientData();
+        await tedisPool.release();
     }
-}
 
-faz().then(() => {
-    console.log("Done");
-}).catch(reason => {
-    console.error("FIRST LEVEL THROW", reason);
-})
+    expect(3).toBe(3);
+});
+
