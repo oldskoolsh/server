e9ba96bcf13c43cbe2b58ecba36df5c6645c60a0
Ricardo Pardini <ricardo@pardini.net>
2020-11-20 17:39:08 +0100

    more fields

--- src/expander_merger/expandermerger.ts
+++ src/expander_merger/expandermerger.ts
@@ -30,6 +30,12 @@ export interface StandardCloudConfig {
     apt?: any;
     packages?: any[];
     final_message?: string;
+    resize_rootfs?: boolean;
+    disable_root?: boolean;
+    package_update?: boolean;
+    package_reboot_if_required?: boolean;
+    package_upgrade?: boolean;
+    ssh_authorized_keys?: string[];
 }
 
 export interface ExtendedCloudConfig extends StandardCloudConfig {
@@ -244,10 +250,7 @@ export class CloudInitExpanderMerger {
         try {
             await this.processOneRun();
 
-            // since that worked, invoke the processor stack; @TODO: context could modify the processor stack.
-            if (!this.currentMerger.cloudConfig) {
-                console.warn("here", this.currentMerger.cloudConfig);
-            }
+            // since that worked (did not throw), invoke the processor stack; context could modify the processor stack.
             const processedCloudConfig: StandardCloudConfig = await new CloudInitProcessorStack(this.context, this.repoResolver, this.currentMerger.cloudConfig)
                 .addDefaultStack()
                 .process();
